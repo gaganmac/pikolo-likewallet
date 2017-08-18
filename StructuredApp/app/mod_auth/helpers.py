5d25122619a2c209f3448b80f0c43cb1fe750f08
Josh Goldman <joshgoldman@berkeley.edu>
2017-08-18 16:43:25 -0700

    number format fix

--- StructuredApp/app/mod_auth/helpers.py
+++ StructuredApp/app/mod_auth/helpers.py
@@ -2,13 +2,13 @@ def truncate(num):
 	if num > 1000000:
 		num = round(num, -6)
 		num = str(num)
-		num = num[:-6].upper()
+		num = num[:-6]
 		num = num + 'm'
 		return num
 	elif num > 1000:
 		num = round(num, -3)
 		num = str(num)
-		num = num[:-3].upper()
+		num = num[:-3]
 		num = num + 'k'
 		return num
 	else:
