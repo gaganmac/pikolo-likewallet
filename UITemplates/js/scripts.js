2f6577d576ce314ff51994f1ad84a317f74d3656
Juan Ordonez <juan@gopikolo.com>
2017-08-22 16:44:08 -0700

    Added new post modals
    Still work in progress

--- UITemplates/js/scripts.js
+++ UITemplates/js/scripts.js
@@ -1,20 +1,27 @@
 function main(){
 
 	//DASHBOARD PAGE
-	$("#posts").on("activate.bs.scrollspy", function () {
-	  	alert("test");
-	  	//Open modal for new post
-		$("#modal-new-post").modal("show");
-		$('.modal-backdrop').appendTo("#001");   
-		//remove the padding right and modal-open class from the body tag which bootstrap adds when a modal is shown
-		$('body').removeClass("modal-open")
-		$('body').css("padding-right","");
-		//Close modal when cost is submitted
-		$(document).on("submit", "#form-cost", function(){
-			$(".modal").modal("hide");
-			return false;
+
+  	//Automatically open modals for new posts 
+	$(".modal-new-post").modal("show"); 
+	$(".modal-backdrop").appendTo(".new-post");
+	/*$(".modal-new-post").on("show.bs.modal", function(){
+		var $newPost = $(this).parentsUntil(".row").children(".new-post");
+		$(".modal-backdrop").appendTo($newPost); 
+	});*/
+	//remove the padding right and modal-open class from the body tag which bootstrap adds when a modal is shown
+	$('body').removeClass("modal-open")
+	$('body').css("padding-right","");
+
+	//Close and remove modal from DOM when cost is submitted 
+	$(document).on("submit", ".form-cost", function(){
+		$(this).parentsUntil(".row").children(".post").removeClass("new-post");
+		$(this).parentsUntil(".post", ".modal-new-post").modal("hide");
+		$(this).parentsUntil(".row", ".modal-new-post").on("hidden.bs.modal", function(){
+			$(this).remove();
 		});
-	})
+		return false;
+	});
 
 	// Add smooth scrolling on all links inside dashboard navigation
 	$("#nav-dashboard a").on('click', function(event) {
@@ -44,18 +51,34 @@ function main(){
 
 	//INFLUENCERS PAGE
 
-	//Hide elements
+	//Hide or disable elements
 	$(".add-group-menu").hide();
+	$("#btn-groups").attr("disabled", true);
 
 
 	//Add new influencer (e.g.Selena Gomez)
 	$(document).on("click", ".add-influencer-btn", function(){
-		$(".influencer-list").append('<div href="#" class="list-group-item list-group-item-action d-flex justify-content-start flex-nowrap"><img class="img-fluid rounded-circle" src="https://scontent-cdg2-1.cdninstagram.com/t51.2885-19/s320x320/19955667_1078934505542036_6745075692590858240_a.jpg"><div class="d-flex justify-content-start flex-wrap"><div class="influencer-name"><strong>Selena Gomez</strong></div><div class="extra-margin-right"><small>124m followers</small></div><div class="extra-margin-right"><small>1,352 posts</small></div></div><div class="ml-auto d-flex flex-nowrap"><a href="https://www.instagram.com/selenagomez/" target="_blank"><i class="btn-influencer fa fa-instagram"></i><a href="https://www.instagram.com/audreylombard/" target="_blank"><i class="fa fa-facebook btn-influencer"></i></a><a href="https://www.instagram.com/audreylombard/" target="_blank"><i class="fa fa-twitter btn-influencer"></i></a><a href="https://www.instagram.com/audreylombard/" target="_blank"><i class="fa fa-snapchat btn-influencer"></i></a><a class="remove-influencer-btn btn-influencer"><i class="fa fa-trash-o"></i></a>');
+		$("#influencer-list").append('<div href="#" class="list-group-item list-group-item-action d-flex justify-content-start flex-nowrap"><img class="img-fluid rounded-circle" src="https://scontent-cdg2-1.cdninstagram.com/t51.2885-19/s320x320/19955667_1078934505542036_6745075692590858240_a.jpg"><div class="d-flex justify-content-start flex-wrap"><div class="influencer-name"><strong>Selena Gomez</strong></div><div class="extra-margin-right"><small>124m followers</small></div><div class="extra-margin-right"><small>1,352 posts</small></div></div><div class="ml-auto d-flex flex-nowrap"><a href="https://www.instagram.com/selenagomez/" target="_blank"><i class="btn-influencer fa fa-instagram"></i><a href="https://www.instagram.com/audreylombard/" target="_blank"><i class="fa fa-facebook btn-influencer"></i></a><a href="https://www.instagram.com/audreylombard/" target="_blank"><i class="fa fa-twitter btn-influencer"></i></a><a href="https://www.instagram.com/audreylombard/" target="_blank"><i class="fa fa-snapchat btn-influencer"></i></a><a class="remove-influencer-btn btn-influencer"><i class="fa fa-trash-o"></i></a>');
+	});
+
+	//When user has zero influencers, add influencers with file drop from influencers page directly
+	$("#dropzone-zero-influencers").on("drop", function(){
+		$("#container-dz-zero-influencers").hide();
+		$("#influencer-list").delay(300).slideDown();
+		$("#btn-groups").attr("disabled", false);
+	});
+
+	//Or add influencers with file drop from modal
+	$("#dropzone-add-influencer").on("drop", function(){
+		$("#modal-add-influencers").modal("hide");
+		$("#container-dz-zero-influencers").hide();
+		$("#influencer-list").slideDown();
+		$("#btn-groups").attr("disabled", false);
 	});
 
 	//Remove an influencer
 	$(document).on("click", ".remove-influencer-btn", function(){
-		$(this).parentsUntil(".influencer-list")
+		$(this).parentsUntil("#influencer-list")
 			.slideToggle(400, function(){
 				$(this).remove()});
 	});
